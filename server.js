360496ac5e91324a769e9872772b61b0cccd5cda
a-riceeater <elijb12@gmail.com>
2024-01-20 00:20:03 -0800

    comments

--- server.js
+++ server.js
@@ -5,3 +5,12 @@ const app = express();
 app.listen(3005, () => {
     console.log("Listening at http://localhost:3005")
 })
+
+// Add file uploads
+// Encrypt & decrypt files
+// Socket.io or refresh to see new changes to files
+// Optional commit messages to updated files
+// In app with electron it can watch the file selected and then upload to db
+// Other users can manually download the file and edit, then when that file is saved upload etc
+// Location of file is where redonwloaded will be updated - origial user would just replace cloud scores file
+// create backup of original file in case of corrpution/accidental changes
