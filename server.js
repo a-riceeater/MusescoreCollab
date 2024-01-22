e90420724c631fd8e6b41a9af9f2b1a4cd3b4250
a-riceeater <elijb12@gmail.com>
2024-01-21 17:29:39 -0800

    static

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const path = require("path")
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, "static")))
+
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "html", "index.html"))
 })
