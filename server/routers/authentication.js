7bfe5bdca6f4e7944a0eb36cc00cc6cb14fa1144
a-riceeater <elijb12@gmail.com>
2024-01-21 17:46:53 -0800

    file

--- server/routers/authentication.js
+++ server/routers/authentication.js
@@ -3,6 +3,8 @@ const path = require("path")
 
 const app = express.Router();
 
-app.get("/login")
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "../","../","html","auth","login.html"))
+})
 
 module.exports = app;
